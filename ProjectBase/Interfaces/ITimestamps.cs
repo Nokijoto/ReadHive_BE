dc77a945ba58f5c55e71cc02bcd3a8c1079ec7c8
Nokijoto <nokijoto@protonmail.com>
2024-09-17 21:11:17 +0200

    wip

---
+++ ProjectBase/Interfaces/ITimestamps.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectBase.Interfaces;
+
+public interface ITimestamps
+{
+    public DateTime? CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+}
