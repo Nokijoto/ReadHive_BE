d8f340ba513e46ad4197b6a84fcd8e24afef903c
Nokijoto <nokijoto@protonmail.com>
2024-10-05 16:09:51 +0200

    update

---
+++ BookService/Book.Application/Exceptions/ObjectNotExistException.cs
@@ -0,0 +1,20 @@
+﻿namespace Application.Exceptions;
+
+public class ObjectNotExistException : Exception
+{
+    public ObjectNotExistException()
+        : base("Object does not exist.") 
+    {
+    }
+
+    public ObjectNotExistException(string message)
+        : base(message) 
+    {
+    }
+
+    public ObjectNotExistException(string message, Exception innerException)
+        : base(message, innerException) 
+    {
+    }
+    
+}
